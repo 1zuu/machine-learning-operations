3496057ecb182f6eb90bbf1702422243f92d816a
Isuru Alagiyawanna <isurualagiyawanna9717@gmail.com>
2022-05-30 09:03:58 +0530

    Initialize File Structure

---
+++ README.md
@@ -0,0 +1,13 @@
+***************** CREATE CONDA ENV *****************
+
+conda create -n mlops python=3.7 -y
+activate mlops
+
+pip install -r requirements.txt
+
+
+***************** DVC Initialization *****************
+
+git init
+dvc init
+dvc add data_git/winequality.csv
---
+++ template.py
@@ -0,0 +1,29 @@
+import os 
+
+dirs = [
+    os.path.join('data', 'raw'),
+    os.path.join('data', 'processed'),
+    'notebooks',
+    'saved_models',
+    'src'
+]
+
+for d in dirs:
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+    with open(os.path.join(d, '.gitkeep'), 'w') as f:
+        pass
+
+files = [
+    'dvc.yaml',
+    'params.yaml',
+    '.gitignore',
+    os.path.join('src', '__init__.py'),
+
+
+]
+
+for f in files:
+    with open(f, 'w') as f:
+        pass
